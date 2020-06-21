411403334f74b5ab41bf558227af23710e5d4181
Edward Elric <SASUKE688848@gmail.com>
2020-06-21 20:15:25 +0800

    feat: add utility functions for Vec3

--- src/tracer/vec3.rs
+++ src/tracer/vec3.rs
@@ -1,5 +1,5 @@
 use std::f32::{MAX as f32_MAX, MIN as f32_MIN};
-use std::ops::{Add, Div, Mul, Sub};
+use std::ops::{Add, Div, Mul, Neg, Sub};
 
 #[derive(Clone, Copy, Default, Debug, PartialEq)]
 pub struct Vec3 {
@@ -56,9 +56,33 @@ impl Div for Vec3 {
     }
 }
 
+impl Neg for Vec3 {
+    type Output = Vec3;
+
+    fn neg(self) -> Self::Output {
+        Vec3 {
+            x: -self.x,
+            y: -self.y,
+            z: -self.z,
+        }
+    }
+}
+
 impl Vec3 {
     pub fn new(x: f32, y: f32, z: f32) -> Vec3 {
         Vec3 { x, y, z }
+    }
+
+    pub fn cross(u: Vec3, v: Vec3) -> Vec3 {
+        Vec3 {
+            x: u.y * v.z - u.z * v.y,
+            y: u.z * v.x - u.x * v.z,
+            z: u.x * v.y - u.y * v.x,
+        }
+    }
+
+    pub fn dot(u: Vec3, v: Vec3) -> f32 {
+        u.x * v.x + u.y * v.y + u.z * v.z
     }
 }
 
